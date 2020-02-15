43324da692b5f5b09aa1eb2f97b70e2e67d1196c
Mike Christofilopoulos <mhristof@gmail.com>
2020-02-15 18:40:11 +0000

    auto update

---
+++ check-for-new-release.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+
+current=$(make version)
+latest=$(curl --silent https://api.github.com/repos/mhristof/alfred-pbpaste/releases | jq '.[].assets[0] | select(.browser_download_url != null) | .browser_download_url' -r)
+
+echo $latest | grep $current &> /dev/null
+
+if [[ $? -ne 0 ]] && [[ ! -z $latest ]]; then
+    echo $latest
+fi
+
+exit 0
