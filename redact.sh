b824db1291ce0483b39e452bf8ed45744f8e796c
Mike Christofilopoulos <mhristof@gmail.com>
2020-02-20 19:26:14 +0000

    fix redact script

--- redact.sh
+++ redact.sh
@@ -1,9 +1,6 @@
 #! /usr/bin/env bash
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-#set -euo pipefail
-#IFS=$'\n\t'
 
-while read -r line; do
+pbpaste | while IFS= read line; do
     aws_key=$(echo $line | grep -oP '(?<![A-Z0-9])[A-Z0-9]{20}(?![A-Z0-9])')
     if [[ ! -z $aws_key ]]; then
         line=$(echo $line | sed "s/$aws_key/${aws_key:0:5}xxxxxxxxxx${aws_key:15:20}/g")
@@ -13,7 +10,8 @@ while read -r line; do
     if [[ ! -z $aws_secret ]]; then
         line=$(echo $line | sed "s!${aws_secret}!${aws_secret:0:5}xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx${aws_secret:35:40}!g")
     fi
+
     echo $line
-done < <(pbpaste)
+done
 
 exit 0
